94516c0d2bfb21a26a85f572c34d05fc4dbeab17
gehadsamy <61977855+gehadsamy@users.noreply.github.com>
2021-10-24 19:26:00 +0200

    Update NavBar.jsx

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -31,11 +31,13 @@ function NavBar () {
   return (
     <>
       <div style={{'display': 'flex', 'justifyContent':'center', 'margin': '1rem'}}>
-        <Tabs value={currentTab} style={{'width': 'fit-content'}} >
+              <Tabs value={currentTab} style={{'width': 'fit-content'}} >
           <Tab label="Ecommerce Page" href="/products" value="/products" to="/products"  component={Link}/>
           <Tab label="Gehad protofolio" href="/" value="/" to="/" component={Link} />
           <Tab label="Simple Counter" value="/Counter" to="/Counter" href="/Counter" component={Link}/>
           <Tab label="Todo list" value="/Todo" to="/Todo" href="/Todo" component={Link}/>
+          <Tab label="Login" value="/Login" to="/Login" href="/Login" component={Link}/>
+          <Tab label="Registration" value="/Registration" to="/Registration" href="/Registration" component={Link}/>
         </Tabs>
       </div>
 
